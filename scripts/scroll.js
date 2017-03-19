fa10affcb53a828c7f64b075e7e49f8a0f8a1427
Home <neebjustin@gmail.com>
2017-03-19 08:29:30 -0400

    parallax

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -1,5 +1,7 @@
 (function scroll() {
 	var scroll = document.getElementsByClassName("scroll");
+	var fade = document.getElementsByClassName("fade");
+	var lax = document.getElementsByClassName("lax");
 	window.onscroll = function() {
 		if (window.pageYOffset > 0) {
 			for (var i = scroll.length - 1; i >= 0; i--) {
@@ -10,6 +12,13 @@
 			for (var i = scroll.length - 1; i >= 0; i--) {
 				scroll[i].classList.remove("scrolled");
 			}
+		}
+		// .5 cushion for missed scroll events
+		if ((window.pageYOffset / (window.innerHeight / 3)) <= 1.5) {
+			fade[0].style.opacity = 1 - (window.pageYOffset / (window.innerHeight / 3));
+		}
+		if (window.pageYOffset <= window.innerHeight) {
+			lax[0].style.backgroundPosition = "0% " + (((window.pageYOffset / window.innerHeight) * 50) + 50) + "%";
 		}
 	}
 }());
