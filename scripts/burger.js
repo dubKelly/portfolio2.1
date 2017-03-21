2aeaba393b63bb189d49c1f98332bf4d109cd464
Home <neebjustin@gmail.com>
2017-03-21 10:02:28 -0400

    mobileNav

--- scripts/burger.js
+++ scripts/burger.js
@@ -1,6 +1,19 @@
 (function burger() {
 	var gBurg = document.getElementsByClassName("goodBurger");
+	var nav = document.getElementsByClassName("mobileNav");
+	var toggle = 0;
 	gBurg[0].onclick = function() {
-		this.classList.toggle("open");
+			this.classList.toggle("open");
+			nav[0].classList.toggle("open");
+		if (toggle === 0) {
+			toggle++;
+			nav[0].style.zIndex = "2";
+		}
+		else {
+			toggle--;
+			setTimeout(function() {
+				nav[0].style.zIndex = "-1";
+			}, 200);
+		}
 	}
 }());
