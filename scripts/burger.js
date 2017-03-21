3d47aa555758cf6a54799abce2cc58ef5dae69c8
Home <neebjustin@gmail.com>
2017-03-21 10:19:33 -0400

    blur

--- scripts/burger.js
+++ scripts/burger.js
@@ -1,10 +1,14 @@
 (function burger() {
 	var gBurg = document.getElementsByClassName("goodBurger");
 	var nav = document.getElementsByClassName("mobileNav");
+	var blur = document.getElementsByClassName("blur");
 	var toggle = 0;
 	gBurg[0].onclick = function() {
 			this.classList.toggle("open");
 			nav[0].classList.toggle("open");
+			for (var i = blur.length - 1; i >= 0; i--) {
+				blur[i].classList.toggle("open");
+			}
 		if (toggle === 0) {
 			toggle++;
 			nav[0].style.zIndex = "2";
