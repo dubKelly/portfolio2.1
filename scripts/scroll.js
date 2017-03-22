b8f3ad109294c03facaf8166011380cd0fea9e24
Home <neebjustin@gmail.com>
2017-03-21 22:05:54 -0400

    scale up/aced lax

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -1,9 +1,23 @@
 (function scroll() {
 	var scroll = document.getElementsByClassName("scroll");
 	var fade = document.getElementsByClassName("fade");
-	var lax = document.getElementsByClassName("lax");
+	// var lax = document.getElementsByClassName("lax");
+	var latestScroll = 0;
+	var ticking = false;
 	window.onscroll = function() {
-		if (window.pageYOffset > 0) {
+		latestScroll = window.pageYOffset;
+		requestTick();
+	}
+	function requestTick() {
+		if (!ticking) {
+			requestAnimationFrame(paint);
+		}
+		ticking = true;
+	}
+	function paint() {
+		ticking = false;
+		var currentScroll = latestScroll;
+		if (currentScroll > 0) {
 			for (var i = scroll.length - 1; i >= 0; i--) {
 				scroll[i].classList.add("scrolled");
 			}
@@ -14,11 +28,11 @@
 			}
 		}
 		// .5 cushion for missed scroll events
-		if ((window.pageYOffset / (window.innerHeight / 3)) <= 1.5) {
-			fade[0].style.opacity = 1 - (window.pageYOffset / (window.innerHeight / 3));
+		if ((currentScroll / (window.innerHeight / 3)) <= 1.5) {
+			fade[0].style.opacity = 1 - (currentScroll / (window.innerHeight / 3));
 		}
-		if (window.pageYOffset <= window.innerHeight) {
-			lax[0].style.backgroundPosition = "0% " + (((window.pageYOffset / window.innerHeight) * 50) + 50) + "%";
-		}
+		// if (currentScroll <= window.innerHeight) {
+		// 	lax[0].style.backgroundPosition = "0% " + (((currentScroll / window.innerHeight) * 50) + 50) + "%";
+		// }
 	}
 }());
