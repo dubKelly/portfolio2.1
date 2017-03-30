a0c6649458cf9ae1e99c788639a1b9659f9cf585
Home <js.neeb1780@gmail.com>
2017-03-30 18:16:27 -0400

    pre about scroll

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -1,11 +1,18 @@
 (function scroll() {
+	/*** DOM ***/
 	var scroll = document.getElementsByClassName("scroll");
 	var fade = document.getElementsByClassName("fade");
-	// var lax = document.getElementsByClassName("lax");
-	var latestScroll = 0;
-	var ticking = false;
 	var mainNav = document.getElementsByClassName("mainNav");
 	var sect = document.getElementsByTagName("section");
+	var windowHeight = window.innerHeight;
+	var pageTop = [0, sect[0].clientHeight, (sect[1].clientHeight + sect[0].clientHeight), (sect[2].clientHeight + sect[1].clientHeight + sect[0].clientHeight)];
+	var latestScroll = 0;
+	var ticking = false;
+	// var lax = document.getElementsByClassName("lax");
+	window.onresize = function xY() {
+		windowHeight = window.innerHeight;
+		pageTop = [0, sect[0].clientHeight, (sect[1].clientHeight + sect[0].clientHeight), (sect[2].clientHeight + sect[1].clientHeight + sect[0].clientHeight)];
+	}
 	window.onscroll = function() {
 		latestScroll = window.pageYOffset;
 		requestTick();
@@ -21,8 +28,8 @@
 		var currentScroll = latestScroll;
 		/*** addScrolled ***/
 		for (var i = scroll.length - 1; i >= 0; i--) {
-			var y = scroll[i].getAttribute("data-scrollPoint");
-			if (currentScroll > (window.innerHeight * y)) {
+			var scrollPoint = scroll[i].getAttribute("data-scrollPoint");
+			if (currentScroll > (windowHeight * scrollPoint)) {
 				scroll[i].classList.add("scrolled");
 			}
 			else {
@@ -31,11 +38,12 @@
 		}
 		/*** addFade ***/
 		// .5 cushion for missed scroll events
-		if ((currentScroll / (window.innerHeight / 3)) <= 1.5) {
-			fade[0].style.opacity = 1 - (currentScroll / (window.innerHeight / 3));
+		if ((currentScroll / (windowHeight / 3)) <= 1.5) {
+			for (var i = fade.length - 1; i >= 0; i--) {
+				fade[i].style.opacity = 1 - (currentScroll / (windowHeight / 3));
+			}
 		}
 		/*** pagination ***/
-		var pageTop = [0, sect[0].clientHeight, (sect[1].clientHeight + sect[0].clientHeight), (sect[2].clientHeight + sect[1].clientHeight + sect[0].clientHeight)];
 		for (var i = mainNav.length - 1; i >= 0; i--) {
 			if (currentScroll >= pageTop[i] && currentScroll < pageTop[i + 1]) {
 				mainNav[i].classList.add("page");
@@ -45,8 +53,8 @@
 			}
 		}
 		// /*** parallaxBackground ***/
-		// if (currentScroll <= window.innerHeight) {
-		// 	lax[0].style.backgroundPosition = "0% " + (((currentScroll / window.innerHeight) * 50) + 50) + "%";
+		// if (currentScroll <= windowHeight) {
+		// 	lax[0].style.backgroundPosition = "0% " + (((currentScroll / windowHeight) * 50) + 50) + "%";
 		// }
 	}
 }());
